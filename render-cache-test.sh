c315965678498bd5a420a3bf1d33e92d9b2b068c
Keith Lockwood <kwlockwo@gmail.com>
2025-02-07 11:33:26 +1100

    guess we don't use XDG_CACHE_HOME anymore

--- render-cache-test.sh
+++ render-cache-test.sh
@@ -2,18 +2,17 @@
 # exit on error
 set -o errexit
 
+RENDER_CACHE=/opt/render/project/.render
 RENDER_SRC=/opt/render/project/src
 
-echo $XDG_CACHE_DIR
-
 echo "Testing Cache Clear"
-if [[ ! -f $XDG_CACHE_DIR/cache-test/my-cached-file ]]; then
+if [[ ! -f $RENDER_CACHE/cache-test/my-cached-file ]]; then
     echo "No cached file .... creating"
-    mkdir -p $XDG_CACHE_DIR/cache-test
-    echo "File cached" > $XDG_CACHE_DIR/cache-test/my-cached-file
-    cat $XDG_CACHE_DIR/cache-test/my-cached-file
+    mkdir -p $RENDER_CACHE/cache-test
+    echo "File cached" > $RENDER_CACHE/cache-test/my-cached-file
+    cat $RENDER_CACHE/cache-test/my-cached-file
 else
-    cat $XDG_CACHE_DIR/cache-test/my-cached-file
+    cat $RENDER_CACHE/cache-test/my-cached-file
 fi
 
 echo "Testing NextJS cache"
